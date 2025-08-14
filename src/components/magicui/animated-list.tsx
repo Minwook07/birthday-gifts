39dadbbf87749a20f9d30e82e9c776eec9c627da
Minwook07 <mith.chett@gmail.com>
2025-08-14 15:49:26 +0700

    add prop

--- src/components/magicui/animated-list.tsx
+++ src/components/magicui/animated-list.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { AnimatePresence, motion } from "motion/react";
+import { AnimatePresence, HTMLMotionProps, motion } from "motion/react";
 import React, {
   ComponentPropsWithoutRef,
   useEffect,
@@ -9,13 +9,15 @@ import React, {
   useState,
 } from "react";
 
+
 export function AnimatedListItem({ children }: { children: React.ReactNode }) {
-  const animations = {
+  const animations: HTMLMotionProps<"div"> = {
     initial: { scale: 0, opacity: 0 },
     animate: { scale: 1, opacity: 1, originY: 0 },
     exit: { scale: 0, opacity: 0 },
     transition: { type: "spring", stiffness: 350, damping: 40 },
   };
+
 
   return (
     <motion.div {...animations} layout className="mx-auto w-full">
